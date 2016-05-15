e7c2910a347412dcb4ed348a822dc9a0afa7bbd5
Sombrio <chrisparkerrr@gmail.com>
2016-05-15 01:04:54 -0300

    Feito classe de leitura e escrita em disco, adicionado testes da mesma classe.

---
+++ RecoveryDB/DiskData.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecoveryDB
+{
+    public class DiskData
+    {
+        public List<DiskRow> diskRow = new List<DiskRow>();
+        string filePath = "DiskData.txt";
+        public DiskData()
+        {
+            LoadDiskData();
+        }
+        private StreamReader streamReader
+        {
+            get
+            {
+                return new StreamReader(filePath);
+            }
+        }
+
+        private StreamWriter streamWriter
+        {
+            get
+            {
+                return new StreamWriter(filePath, false);
+            }
+        }
+
+        private void LoadDiskData()
+        {
+            diskRow.Clear();
+            var reader = streamReader;
+            string currentLine;
+            while((currentLine = reader.ReadLine()) != null)
+            {
+                var split = currentLine.Split(',');
+
+                int id = int.Parse(split[0]);
+                string name = split[1];
+                double salary = double.Parse(split[2]);
+
+                var row = new DiskRow()
+                {
+                    ID = id,
+                    Name = name,
+                    Salary = salary
+                };
+
+                diskRow.Add(row);
+            }
+            reader.Close();
+        }
+
+        public void SaveDiskData()
+        {
+            var writer = streamWriter;
+            diskRow.ForEach(x => writer.WriteLine(x.ID + "," + x.Name + "," + x.Salary));
+            writer.Close();
+        }
+
+        public DiskRow GetRowById(int id)
+        {
+            LoadDiskData();
+            return diskRow.Single(x => x.ID.Equals(id));
+        }
+
+        public double GetSalaryById(int id)
+        {
+            LoadDiskData();
+            return diskRow.Single(x => x.ID.Equals(id)).Salary;
+        }
+
+        public void SetSalaryById(int id, double salary)
+        {
+            diskRow.Single(x => x.ID.Equals(id)).Salary = salary;
+            SaveDiskData();
+            LoadDiskData();
+        }
+        public void SetNameById(int id, string name)
+        {
+            diskRow.Single(x => x.ID.Equals(id)).Name = name;
+            SaveDiskData();
+            LoadDiskData();
+        }
+    }
+}
---
+++ RecoveryDBTest/TextFileTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecoveryDB;
+using FluentAssertions;
+
+namespace RecoveryDBTest
+{
+    [TestClass]
+    public class TextFileTest
+    {
+        [TestMethod]
+        public void ReadID()
+        {
+            var diskData = new DiskData();
+            diskData.GetRowById(1).ID.Should().Be(1);
+        }
+        [TestMethod]
+        public void ReadName()
+        {
+            var diskData = new DiskData();
+            diskData.GetRowById(1).Name.Should().Be("Maria");
+        }
+        [TestMethod]
+        public void ReadSalary()
+        {
+            var diskData = new DiskData();
+            diskData.GetRowById(1).Salary.Should().Be(500);
+        }
+
+        [TestMethod]
+        public void WriteSalary()
+        {
+            var diskData = new DiskData();
+            diskData.SetSalaryById(3, 700.00);
+            diskData.GetRowById(3).Salary.Should().Be(700.00);
+        }
+        [TestMethod]
+        public void WriteName()
+        {
+            var diskData = new DiskData();
+            diskData.SetNameById(3, "Pedro");
+            diskData.GetRowById(3).Name.Should().Be("Pedro");
+        }
+    }
+}
